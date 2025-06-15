baa675fdbb8c6174c29922ca79c105fd14061f90
firasbelhiba <firas.belhiba@esprit.tn>
2025-06-15 23:33:56 +0100

    Removing required from transaction model

--- models/Transaction.js
+++ models/Transaction.js
@@ -6,7 +6,7 @@ const transactionSchema = new mongoose.Schema({
   type: { type: String, enum: ['income', 'expense'], required: true },
   amount: { type: Number, required: true, min: 0 }, // store as cents if needed
   category: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String, default: '' },
   date: { type: Date, default: Date.now },
 }, { timestamps: true });
 
