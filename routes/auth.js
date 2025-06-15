9129db0528a645edfca3ab9ec4736f662e776d92
firasbelhiba <firas.belhiba@esprit.tn>
2025-06-15 16:41:45 +0100

    Fixing login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, verifyToken } = require('../controllers/authController');
 const auth = require('../middleware/auth');
 
 router.post('/register', register);
